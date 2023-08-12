064374b8211de43c3de06a464927f9ebaced0b81
Zsolt Marku <zsolt.marku910221@gmail.com>
2023-08-12 18:35:47 +0200

    add material UI dependency and design header

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import DeleteIcon from '@mui/icons-material/Delete';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 const Note = (props) => {
 
-const handleClick = () => {
+  const handleClick = () => {
     props.onDelete(props.id)
-}
+  }
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={handleClick}>DELETE</button>
+
+      <Zoom in={true}>
+        <Fab onClick={handleClick}>
+          <DeleteIcon />
+        </Fab>
+      </Zoom>
     </div>
   );
 }
